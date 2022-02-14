8a41f8504ff241fb06fc15a2b4b8d049d98cd29b
IlyaBalykov <balykov.ilya.88@gmail.com>
2022-02-14 18:00:27 +0300

    feat: add translation

---
+++ js/index.js
@@ -0,0 +1,14 @@
+import i18Obj from "./translate.js";
+
+const dataAttr = document.querySelectorAll('[data-i18n]')
+const lang = document.querySelector('.lang-list')
+
+window.onload = getTranslate
+lang.addEventListener('change', getTranslate)
+
+function getTranslate() {
+  dataAttr.forEach((element, index) => {
+    element.textContent = ""
+    element.textContent = i18Obj[lang.value][element.dataset.i18n]
+    })
+}
---
+++ js/translate.js
@@ -0,0 +1,30 @@
+const i18Obj = {
+  'ENG': {
+    'cv': 'CV',
+    'links': 'Links',
+    'news': 'News',
+    'contacts': 'Contacts',
+    'hero-title': 'Kristina Balykova',
+    'download': 'Download',
+    'show-cv': 'Show CV',
+  },
+  'POR': {
+    'cv': 'CV',
+    'links': 'Links',
+    'news': 'Notícias',
+    'contacts': 'Contatos',
+    'hero-title': 'Kristina Balykova',
+    'download': 'Download',
+    'show-cv': 'Mostrar currículo',
+  },
+  'RUS': {
+    'cv': 'Резюме',
+    'links': 'Ссылки',
+    'news': 'Новости',
+    'contacts': 'Контакты',
+    'hero-title': 'Кристина Балыкова',
+    'download': 'Загрузить',
+    'show-cv': 'Показать резюме',
+  }
+}
+export default i18Obj;
