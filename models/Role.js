5126c1fb28fe770993f05512d4d4803e1c581a35
Ilya Balykov <balykov.ilya.88@gmail.com>
2022-06-04 19:27:08 +0300

    refactor: move project files to /src

--- models/Role.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const Role = new Schema( {
-    value: { type: String, unique: false, default: 'user' },
-})
-
-module.exports = model('Role', Role) //model name, template of model
